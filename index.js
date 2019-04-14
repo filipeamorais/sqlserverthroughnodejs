339a4dd28e83b2eb07512d170a26c51471575e31
Filipe de Andrade Morais <afilipem@gmail.com>
2019-04-13 20:46:29 -0500

    bufix: adding the necessary changes to be able to connect to the database

--- index.js
+++ index.js
@@ -1,40 +1,72 @@
-var Connection = require('tedious').Connection;
+var Connection = require('tedious').Connection; 
 var Request = require('tedious').Request;
-fs=require('fs');
+fs = require('fs');
+var sql = require ('mssql/msnodesqlv8').Connection;
+
 
 var data = fs.readFileSync('./login_info');
 data = data.toString();
 
-var arr = data.split(",")
-var myUsername = arr [0];
-var myPassword = arr [1];
-var myDbname = arr [2];
+var arr = data.split(",");
+var myUsername = arr[0] + "@mydbserverfam";
+var myPassword = arr[1];
+var myDbname = arr[2];
 var myServername = arr[3] + ".database.windows.net";
+console.log(myUsername+' '+myPassword+' '+myDbname+' '+myServername)
 
 //Connection parameters to database
-config={
-    server:myServername,
-    userName:myUsername,
-    password:myPassword,
-    options:{
-        database:myDbname,
-        encrypt:true
-    }
+
+// var config={
+//     server: 'mydbserverfam.database.windows.net',
+//     options: { 
+//         userName: 'filipeam',
+//         password: '0dQYS4xivBst',
+//         encrypt:true,
+//         database:'MyDB'
+//     }
+// }   
+ var config = 
+ {
+    authentication: {
+        options: {
+	        userName:  myUsername,
+            password:  myPassword,
+        },
+        type: 'default'
+    },
+    server: myServername,
+	options: {
+		database:  myDbname,
+		encrypt: true
+	}
 }
 
-var connection=new Connection(config);
+
+var connection = new Connection(config);
+console.log(connection);
 connection.on('connect',function(err){
     if(err){
         console.log(err)
     }else{
+        console.log("Connected")
         queryDatabase()
     }
 });
 
+// var conn = new sql(config);
+// console.log(conn);
+// conn.connect(function (err) {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+// });
+
+
 function queryDatabase(){
     console.log('Reading rows from the Table...');
     
-    var request = new Resquest(
+    var request = new Request(
         "SELECT * FROM Persons",
         function(err, rowCount, rows){
             console.log(rowCount+'row(s) returned');
